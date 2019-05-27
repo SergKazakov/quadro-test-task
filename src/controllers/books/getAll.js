43594ca8997f3356ed779cfc933775162c768ba4
Sergey Kazakov <sergeykazakoff@yandex.ru>
2019-05-27 18:24:10 +0300

    :sparkles: Add filter and sorting

--- src/controllers/books/getAll.js
+++ src/controllers/books/getAll.js
@@ -8,16 +8,72 @@ const schema = Joi.object({
   count: Joi.number()
     .integer()
     .min(0)
-    .default(10),
+    .default(100),
+  filter: Joi.object({
+    title: Joi.string().trim(),
+    description: Joi.string().trim(),
+    image: Joi.string().trim(),
+    author: Joi.string().uuid(),
+    created_at: Joi.object({
+      gte: Joi.date(),
+      lte: Joi.date(),
+    }),
+  }).default({}),
+  sorting: Joi.array()
+    .items(
+      Joi.array().ordered(
+        Joi.string()
+          .valid("title", "description", "image", "created_at")
+          .required(),
+        Joi.string()
+          .valid(["asc", "desc"])
+          .required(),
+      ),
+    )
+    .min(1)
+    .default([["created_at", "desc"]]),
 }).min(1)
 
 exports.getAll = async ctx => {
-  const { page, count } = await schema.validate(ctx.query)
+  const { page, count, filter, sorting } = await schema.validate(ctx.query)
+
+  const where = Object.entries(filter)
+    .reduce((res, [key, value]) => {
+      if (["title", "description", "image"].includes(key)) {
+        return [...res, `${key} like lower('%${value}%')`]
+      }
+
+      if (key === "author") {
+        return [...res, `${key} = uuid_to_bin('${value}')`]
+      }
+
+      if (key === "created_at") {
+        const gte = value.gte ? value.gte.toISOString() : undefined
+
+        const lte = value.lte ? value.lte.toISOString() : undefined
+
+        if (gte && lte) {
+          return [...res, `(${key} >= '${gte}' and ${key} <= '${lte}')`]
+        }
+
+        if (gte) {
+          return [...res, `${key} >= '${gte}'`]
+        }
+
+        if (lte) {
+          return [...res, `${key} <= '${lte}'`]
+        }
+      }
+
+      return res
+    }, [])
+    .join(" and ")
 
   const [[{ total }]] = await ctx.db.raw(
     `
       select count(*) total
       from books
+      ${where ? `where ${where}` : ""}
     `,
   )
 
@@ -38,7 +94,8 @@ exports.getAll = async ctx => {
         created_at,
         updated_at
       from books
-      order by created_at desc
+      ${where ? `where ${where}` : ""}
+      order by ${sorting.map(([x, y]) => `${x} ${y}`).join(",")}
       limit :limit offset :offset
     `,
     { offset: (page - 1) * count, limit: count },
--- src/index.js
+++ src/index.js
@@ -2,11 +2,33 @@ const knex = require("knex")
 const Koa = require("koa")
 const compose = require("koa-compose")
 const bodyParser = require("koa-bodyparser")
+const qs = require("qs")
+const merge = require("merge-descriptors")
 
 const knexConfig = require("./knexfile")
 const { routes } = require("./routes")
 
 const app = new Koa()
+
+const qsOptions = { strictNullHandling: true }
+
+merge(app.request, {
+  get query() {
+    const str = this.querystring
+
+    this._querycache = this._querycache || {}
+
+    if (!this._querycache[str]) {
+      this._querycache[str] = qs.parse(str, qsOptions)
+    }
+
+    return this._querycache[str]
+  },
+
+  set query(obj) {
+    this.querystring = qs.stringify(obj, qsOptions)
+  },
+})
 
 app.context.db = knex(knexConfig[process.env.NODE_ENV || "development"])
 
